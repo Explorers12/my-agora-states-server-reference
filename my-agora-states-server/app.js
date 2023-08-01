a8498660b1d5f5667f9718c5734b8fe268d9c6e3
Explorers12 <gva05134@naver.com>
2023-08-01 16:20:20 +0900

    일단2

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -1,29 +1,33 @@
-const express = require('express');
+import express, { json } from "express";
 const app = express();
 
-const cors = require('cors');
-const morgan = require('morgan');
+import cors from "cors";
+import morgan from "morgan";
 
 // TODO: cors를 적용합니다.
 app.use(cors());
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-app.use(express.json());
+app.use(json());
 // OPTIONAL: HTTP 요청 logger인 morgan을 적용합니다.
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 
 const port = 3001;
-const discussionsRouter = require('./router/discussions');
+import discussionsRouter from "./router/discussions";
 
-// TODO: /discussions 경로로 라우팅합니다. 
-app.use('/discussions', discussionsRouter);
+// TODO: /discussions 경로로 라우팅합니다.
+app.use("/discussions", discussionsRouter);
 
-app.get('/', (req, res) => {
-  res.status(200).send('Welcome, My Agora States Server!');
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome, My Agora States Server!");
 });
 
 const server = app.listen(port, () => {
   console.log(`[RUN] My Agora States Server... | http://localhost:${port}`);
 });
 
-module.exports.app = app;
-module.exports.server = server;
+const _app = app;
+export { _app as app };
+const _server = server;
+export { _server as server };
