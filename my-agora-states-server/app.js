f153b517ace5ed9a6ff6aa901fbe4da266f72a6f
Explorers12 <gva05134@naver.com>
2023-08-01 16:25:40 +0900

    일단2

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const app = express();
 
-import cors from "cors";
-import morgan from "morgan";
+const cors = require("cors");
+const morgan = require("morgan");
 
 // TODO: cors를 적용합니다.
 app.use(cors());
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
-app.use(json());
+app.use(express.json());
 // OPTIONAL: HTTP 요청 logger인 morgan을 적용합니다.
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
 
 const port = 3001;
-import discussionsRouter from "./router/discussions";
+const discussionsRouter = require("./router/discussions");
 
 // TODO: /discussions 경로로 라우팅합니다.
 app.use("/discussions", discussionsRouter);
@@ -27,7 +27,5 @@ const server = app.listen(port, () => {
   console.log(`[RUN] My Agora States Server... | http://localhost:${port}`);
 });
 
-const _app = app;
-export { _app as app };
-const _server = server;
-export { _server as server };
+module.exports.app = app;
+module.exports.server = server;
